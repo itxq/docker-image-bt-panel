82af8b5fd685008b16740a314056105b8f1377ed
IT小强xqitw.cn <mail@xqitw.cm>
2019-11-20 19:14:54 +0800

    shell脚本调整

--- shell/run.sh
+++ shell/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# 执行自定义脚本
-/bin/bash /itxq/shell/shell.sh
+# 启动 crond
+/usr/sbin/crond start
 # 启动宝塔
 /etc/init.d/bt restart
+# 执行自定义脚本
+/bin/bash /itxq/shell/shell.sh
 # 维持容器状态
 tail -f -n 1 /itxq/shell/run.log
--- shell/shell.sh
+++ shell/shell.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-# 启动 crond
-/usr/sbin/crond start
