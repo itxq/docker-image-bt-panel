79f4942390bc689a94a56ee9c6810c0a46deaaaf
IT小强xqitw.cn <mail@xqitw.cm>
2019-11-21 13:40:25 +0800

    脚本优化

--- shell/run.sh
+++ shell/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# 启动 crond
-/usr/sbin/crond start
-# 启动宝塔
-/etc/init.d/bt restart
+
 # 执行自定义脚本
 /bin/bash /itxq/shell/shell.sh
+# 启动 crond
+rm -rf /var/run/crond.pid
+/usr/sbin/crond restart
+# 启动宝塔
+/etc/init.d/bt restart
 # 维持容器状态
 tail -f -n 1 /itxq/shell/run.log
